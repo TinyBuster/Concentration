ca6dd760d5ce035a9f78707830b3e6eb8fd1dbb5
Jiawen(Kevin) Bai <bai@jiawens-macbook-pro.local>
2018-01-04 14:31:13 -0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Jiawen Bai on 1/4/18.
+//  Copyright © 2018 Jiawen Bai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
